048935f29d7ff901bceac0ee1c7eb77e9f5d4139
elpidiocabral <netoelpidio1@gmail.com>
2025-01-18 21:51:24 -0300

    chore\correção-main.lua

---
+++ main.lua
@@ -0,0 +1,37 @@
+local Menu = require("src.states.menu")
+local Settings = require("src.states.settings")
+local Gameplay = require("src.states.gameplay")
+
+-- Gerenciador de estados do jogo
+local currentState = nil
+
+-- Função global para alterar o estado do jogo
+function changeState(newState)
+    currentState = newState
+    if currentState and currentState.load then
+        currentState.load()
+    end
+end
+
+function love.load()
+    -- Inicializa o jogo no menu
+    changeState(Menu)
+end
+
+function love.update(dt)
+    if currentState and currentState.update then
+        currentState.update(dt)
+    end
+end
+
+function love.draw()
+    if currentState and currentState.draw then
+        currentState.draw()
+    end
+end
+
+function love.keypressed(key)
+    if currentState and currentState.keypressed then
+        currentState.keypressed(key)
+    end
+end
