ca0bf732e94a913facf2c3dfe92ff9e33ff6e456
unknown <netoelpidio1@gmail.com>
2025-01-18 22:11:09 -0300

    chora/utils-keys

---
+++ src/utils/keys.lua
@@ -0,0 +1,17 @@
+Keys {
+    -- Movement
+    up = {"w", "up"},
+    down = {"s", "down"},
+    left = {"a", "left"},
+    right = {"d", "right"},
+    -- Actions
+    interact = "e",
+    attack = "",
+    -- Inventory
+    inventory = "i",
+    -- Menu
+    menu = "escape",
+
+    -- Debug
+    debug = "f1"
+}
